59d4f6a9bfa3d098904b762064c56f4504e60802
jack udema <jacku@missionreadyhq.com>
2024-11-28 14:03:23 +1300

    routes

--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const carValueController=require('../controllers/carValueController')
 
-
-
-
-
-
+router.post('/carvalue',carValueController.getCarValue)
 
 module.exports = router;
